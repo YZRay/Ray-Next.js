d05fb92afed432d19549d175fb1c5b70121d5d32
YZRay <yao19991119@gmail.com>
2023-12-03 18:21:15 +0800

    url型別問題

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,14 +2,14 @@ import { allPosts } from "contentlayer/generated";
 import { MetadataRoute } from "next";
 
 const postsSitemap: MetadataRoute.Sitemap = allPosts.map((post) => ({
-  url: `${process.env.HOST}/blog/${post._raw.flattenedPath}`,
+  url: `${process.env.HOST}/blog/${post._raw.flattenedPath}` || "",
   lastModified: post.date,
 }));
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: process.env.HOST,
+      url: process.env.HOST || "",
       lastModified: new Date(),
     },
     ...postsSitemap,
