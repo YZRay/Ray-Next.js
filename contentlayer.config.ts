63f5674032235014045691c7813ad3e9af025383
YZRay <yao19991119@gmail.com>
2023-12-03 21:36:59 +0800

    新增標題小連結

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -60,38 +60,6 @@ export default makeSource({
       rehypeCodeTitles,
       [
         /**
-         * Adds auto-linking button after h1, h2, h3 headings
-         */
-        rehypeAutolinkHeadings,
-        {
-          behavior: "append",
-          test: ["h2", "h3"],
-          properties: { class: "heading-link" },
-          content: s(
-            "svg",
-            {
-              xmlns: "http://www.w3.org/2000/svg",
-              viewBox: "0 0 24 24",
-              width: "24",
-              height: "24",
-              fill: "none",
-              stroke: "currentColor",
-              "stroke-width": "2",
-              "stroke-linecap": "round",
-              "stroke-linejoin": "round",
-              "aria-label": "Anchor link",
-            },
-            [
-              s("line", { x1: "4", y1: "9", x2: "20", y2: "9" }),
-              s("line", { x1: "4", y1: "15", x2: "20", y2: "15" }),
-              s("line", { x1: "10", y1: "3", x2: "8", y2: "21" }),
-              s("line", { x1: "16", y1: "3", x2: "14", y2: "21" }),
-            ]
-          ),
-        } satisfies Partial<AutolinkOptions>,
-      ],
-      [
-        /**
          * Enhances code blocks with syntax highlighting, line numbers,
          * titles, and allows highlighting specific lines and words
          */
