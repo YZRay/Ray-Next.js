51dd29e6e1c39ef488c88848fe5caf8b9d287614
YZRay <yao19991119@gmail.com>
2023-12-06 21:46:10 +0800

    style: 新增hover效果

--- src/components/PreviousArticle.tsx
+++ src/components/PreviousArticle.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { before } from "node:test";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
 interface Post {
@@ -36,23 +37,35 @@ const PreviousArticle = ({
     : { title: allPosts[0].title, url: allPosts[0].url };
 
   return (
-    <div className="w-10/12 mx-auto py-8 mb-12 border-t-2 border-slate-400 flex">
+    <div className="w-10/12 mx-auto py-8 mb-12 border-t-2 border-slate-400 flex justify-between">
       {prePost && (
-        <Link className="group" href={prePost.url}>
-          <h6 className="font-bold text-slate-800 text-lg">{prePost.title}</h6>
-          <div className="flex items-center gap-2">
-            <IoIosArrowBack className="w-6 h-6 opacity-0 group-hover:opacity-100 transition-all duration-300" />
-            <span className="text-slate-700 text-base">上一篇文章</span>
+        <Link
+          className="group pb-2 relative after:content-[''] after:absolute after:w-full after:left-0 after:scale-x-0 after:h-0.5 after:bg-slate-700 after:transition-transform after:duration-300 after:origin-bottom-left hover:after:scale-x-100 hover:after:origin-bottom-right ease-out"
+          href={prePost.url}
+        >
+          <div className="flex items-center gap-2 pb-2">
+            <IoIosArrowBack className="w-6 h-6 group-hover:text-slate-700 group-hover:-translate-x-2 transition-all duration-300" />
+            <span className="text-slate-700 text-base font-bold">
+              上一篇文章
+            </span>
           </div>
+          <h6 className="font-bold text-slate-800 text-lg">{prePost.title}</h6>
         </Link>
       )}
       {nextPost && (
-        <Link className="group" href={nextPost.url}>
-          <h6 className="font-bold text-slate-800 text-lg">{nextPost.title}</h6>
-          <div className="flex items-center gap-2">
-            <span className="text-slate-700 text-base">下一篇文章</span>
-            <IoIosArrowForward className="w-6 h-6 opacity-0 group-hover:opacity-100 transition-all duration-300" />
+        <Link
+          className="group pb-2 relative after:content-[''] after:absolute after:w-full after:left-0 after:scale-x-0 after:h-0.5 after:bg-slate-700 after:transition-transform after:duration-300 after:origin-bottom-right hover:after:scale-x-100 hover:after:origin-bottom-left ease-out"
+          href={nextPost.url}
+        >
+          <div className="flex items-center gap-2 flex-row-reverse pb-2">
+            <IoIosArrowForward className="w-6 h-6 group-hover:text-slate-700 group-hover:translate-x-2 transition-all duration-300" />
+            <span className="text-slate-700 text-base font-bold">
+              下一篇文章
+            </span>
           </div>
+          <h6 className={`font-bold text-slate-800 text-lg`}>
+            {nextPost.title}
+          </h6>
         </Link>
       )}
     </div>
