0ac3ced1353dcc3cfd8abf573998aa3bd67af964
YZRay <yao19991119@gmail.com>
2023-12-10 19:19:42 +0800

    feat: 新增loading page

---
+++ src/app/loading.tsx
@@ -0,0 +1,16 @@
+import { Spinner } from "@nextui-org/react";
+
+const loading = () => {
+  return (
+    <div className="flex justify-center items-center h-screen w-full">
+      <Spinner
+        size="lg"
+        label="Loading..."
+        color="primary"
+        labelColor="primary"
+      />
+    </div>
+  );
+};
+
+export default loading;
