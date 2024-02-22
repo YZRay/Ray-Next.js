17c5c368d93cc44c786fbd8e1714025913325105
YZRay <yao19991119@gmail.com>
2024-02-22 22:10:57 +0800

    style: 調整文章樣式

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -66,7 +66,7 @@ const PostLayout = ({ params }: { params: { slug: string } }) => {
         </div>
       </aside>
       <div className="w-11/12 md:max-w-3xl mx-auto lg:mx-0 xl:max-w-5xl">
-        <article className="w-10/12 mx-auto py-6 mt-0">
+        <article className="w-10/12 mx-auto py-6 mt-0 prose lg:prose-xl">
           <div className="pb-6 border-b-1 border-neutral-600 dark:border-neutral-100 text-center">
             <h1 className="text-4xl text-neutral-800 dark:text-neutral-300 font-bold mt-2">
               {post.title}
