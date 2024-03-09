df273af27e8792ef8a5609324b5ac50b7c834810
YZRay <yao19991119@gmail.com>
2024-03-09 12:00:15 +0800

    feat: add fetch firebase database

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,4 +1,23 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { getData } from "@/components/api/firebase";
+
 const ProjectPage = () => {
+  const [data, setData] = useState<any>(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const data = await getData();
+        setData(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+    fetchData();
+  }, []);
+  console.log(data);
+
   return <div>ProjectPage</div>;
 };
 
---
+++ src/components/api/firebase.ts
@@ -0,0 +1,13 @@
+import { database } from "../../app/firebaseConfig";
+import { ref, get } from "firebase/database";
+
+export const getData = async () => {
+  try {
+    const headerRef = ref(database, "project");
+    const snapshot = await get(headerRef);
+    return snapshot.val();
+  } catch (error) {
+    console.error("Error getting data:", error);
+    throw error;
+  }
+};
