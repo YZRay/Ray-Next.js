0ea45cc71bc8cdab5a8e0711774948a4a169ded3
YZRay <yao19991119@gmail.com>
2024-03-09 12:51:18 +0800

    feat: add query provider

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,21 +1,14 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { getData } from "@/components/api/firebase";
+import { useQuery } from "@tanstack/react-query";
 
 const ProjectPage = () => {
-  const [data, setData] = useState<any>(null);
+  const { data, isLoading, error, isError } = useQuery({
+    queryKey: ["project"],
+    queryFn: () => getData(),
+  });
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const data = await getData();
-        setData(data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    }
-    fetchData();
-  }, []);
   console.log(data);
 
   return <div>ProjectPage</div>;
