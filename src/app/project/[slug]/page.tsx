5e1dbe80e59dae54a19edf91703234d3ea7e337b
YZRay <yao19991119@gmail.com>
2024-03-19 21:55:13 +0800

    refactor: fetch project

--- src/app/project/[slug]/page.tsx
+++ src/app/project/[slug]/page.tsx
@@ -1,16 +1,23 @@
 import { getData } from "@/components/api/firebase";
+
+async function fetchProject() {
+  try {
+    const data = await getData();
+    return data;
+  } catch (err) {
+    console.error(err);
+    return [];
+  }
+}
+
 export async function generateStaticParams() {
-  const data = await getData()
-    .then((res) => res)
-    .catch((err) => console.error(err));
+  const data = await fetchProject();
   return data.map((post: { slug: string }) => ({
     slug: post.slug,
   }));
 }
 const ProjectPage = async ({ params }: { params: { slug: string } }) => {
-  const data = await getData()
-    .then((res) => res)
-    .catch((err) => console.error(err));
+  const data = await fetchProject();
 
   const project = data.find((item: { slug: string }) => {
     return item.slug === params.slug;
