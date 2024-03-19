d515d00d0c20d076320d535bd9bb59309a16a64e
YZRay <yao19991119@gmail.com>
2024-03-19 22:58:10 +0800

    feat: 新增頁面

--- src/app/project/[slug]/page.tsx
+++ src/app/project/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { getData } from "@/components/api/firebase";
+import GetSkillIcon from "@/components/project/GetSkillIcon";
 
 async function fetchProject() {
   try {
@@ -22,10 +23,27 @@ const ProjectPage = async ({ params }: { params: { slug: string } }) => {
   const project = data.find((item: { slug: string }) => {
     return item.slug === params.slug;
   });
+  console.log(project);
 
   return (
-    <div>
-      <h2>{project.name}</h2>
+    <div className="mx-auto w-3/5">
+      <div className="w-full my-6 md:my-8 lg:my-12">
+        <h2 className="text-center text-xl md:text-3xl">{project.name}</h2>
+        <div>
+          <span>使用技術</span>
+          <div className="flex items-center gap-4">
+            {project?.skill.map((s: string, index: number) => (
+              <div
+                key={index}
+                className="flex gap-1 items-center text-lg lg:text-xl"
+              >
+                {GetSkillIcon(s)}
+                {s}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
