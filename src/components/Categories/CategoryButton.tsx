8922daff57ed48f665f97657a02a39d3e88a2f5c
YZRay <yao19991119@gmail.com>
2024-04-21 21:15:09 +0800

    feat: 調整動畫

--- src/components/Categories/CategoryButton.tsx
+++ src/components/Categories/CategoryButton.tsx
@@ -2,29 +2,61 @@
 import { Link, Button } from "@nextui-org/react";
 import { usePathname } from "next/navigation";
 import clsx from "clsx";
+import { motion } from "framer-motion";
 
 const CategoryButton = ({ slug, text }: { slug: string; text: string }) => {
   const pathname = usePathname();
-
   const slugName = pathname === slug;
 
   return (
-    <Button
-      href={slug}
-      as={Link}
-      radius="full"
-      size="lg"
-      className={clsx(
-        "hover:scale-105 transition-all bg-transparent ease duration-200 border border-darker-600 text-darker-600 dark:border-lighter-100 dark:text-lighter-100 font-medium tracking-widest uppercase",
-        {
-          "bg-darker-400 text-lighter-100 dark:bg-lighter-500 dark:text-darker-200":
-            slugName,
-        }
-      )}
+    <motion.div
+      variants={{
+        hidden: { opacity: 0, scale: 0.5 },
+        visible: {
+          opacity: 1,
+          scale: 1,
+        },
+      }}
+      transition={{ type: "spring" }}
+      exit={{
+        opacity: 1,
+        scale: 1,
+      }}
     >
-      {text}
-    </Button>
+      <Button
+        href={slug}
+        as={Link}
+        radius="full"
+        size="lg"
+        className={clsx(
+          "hover:scale-105 transition-all bg-transparent ease duration-200 border border-darker-600 text-darker-600 dark:border-lighter-100 dark:text-lighter-100 font-medium tracking-widest uppercase",
+          {
+            "bg-darker-400 text-lighter-100 dark:bg-lighter-500 dark:text-darker-200":
+              slugName,
+          }
+        )}
+      >
+        {text}
+      </Button>
+    </motion.div>
   );
 };
 
-export default CategoryButton;
+const CategoryButtonGroup = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <motion.div
+      variants={{
+        hidden: { opacity: 0 },
+        visible: { opacity: 1, transition: { staggerChildren: 0.07 } },
+      }}
+      initial="hidden"
+      animate="visible"
+      exit="hidden"
+      className="flex gap-x-4 gap-y-2 mt-4 mb-6 flex-wrap"
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export { CategoryButton, CategoryButtonGroup };
