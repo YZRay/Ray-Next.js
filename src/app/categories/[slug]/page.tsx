4df2b07c8e25db4232167fec5b7ac9cd9f51057b
YZRay <yao19991119@gmail.com>
2024-04-21 10:16:57 +0800

    新增分類

--- src/app/categories/[slug]/page.tsx
+++ src/app/categories/[slug]/page.tsx
@@ -1,10 +1,68 @@
 import { allPosts } from "contentlayer/generated";
+import { slug } from "github-slugger";
+import { Fragment } from "react";
+import Link from "next/link";
+import { it } from "node:test";
 
-export const generateStaticParams = async () =>
-  allPosts.map((post) => ({ slug: post._raw.flattenedPath }));
+type Params = {
+  params: {
+    slug: string;
+  };
+};
 
-const Category = () => {
-  return <div>page</div>;
+export async function generateStaticParams() {
+  const categories: string[] = [];
+  const paths: [{ slug: string }] = [{ slug: "all" }];
+
+  allPosts.map((item) => {
+    item.tags?.map((tag) => {
+      const trimmedTag = tag.trim();
+      if (!categories.includes(trimmedTag)) {
+        categories.push(trimmedTag);
+        paths.push({ slug: trimmedTag });
+      }
+    });
+  });
+
+  return paths;
+}
+
+const Category = ({ params }: Params) => {
+  const allCategories = ["all"];
+  allPosts.forEach((post) => {
+    post.tags?.forEach((tag) => {
+      const trimmedTag = tag.trim();
+      if (!allCategories.includes(trimmedTag)) {
+        allCategories.push(trimmedTag);
+      }
+    });
+  });
+
+  allCategories.sort();
+
+  const posts = allPosts.filter((post) => {
+    if (params.slug === "all") {
+      return true;
+    }
+    return post.tags?.some((tag) => slug(tag) === params.slug);
+  });
+
+  return (
+    <Fragment>
+      <div className="flex gap-4">
+        {allCategories.map((item, i) => {
+          return (
+            <Link key={i} href={`/categories/${item}`}>
+              {item.toUpperCase()}
+            </Link>
+          );
+        })}
+      </div>
+      {posts.map((item) => {
+        return <div key={item._id}>{item.title}</div>;
+      })}
+    </Fragment>
+  );
 };
 
 export default Category;
