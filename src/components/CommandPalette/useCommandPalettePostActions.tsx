c4f9f1e5f5eedf2a52d7938ed65a0f8b05afeb57
YZRay1119 <yao19991119@mopcon.org>
2024-08-25 12:19:49 +0800

    fix

--- src/components/CommandPalette/useCommandPalettePostActions.tsx
+++ src/components/CommandPalette/useCommandPalettePostActions.tsx
@@ -1,18 +1,18 @@
 "use client";
 import { useRegisterActions } from "kbar";
 import { useRouter } from "next/navigation";
-import { allPosts } from "contentlayer/generated";
+import { getPosts } from "@/posts";
 
-export const useCommandPalettePostActions = () => {
+export const useCommandPalettePostActions = async () => {
   const router = useRouter();
-
+  const posts = await getPosts();
   useRegisterActions(
-    allPosts.map((post) => ({
+    posts.map((post) => ({
       id: post.title,
       name: post.title,
       keywords: post.title,
       shortcut: [],
-      perform: () => router.push(post.url),
+      perform: () => router.push(`/posts/${post.slug}`),
       parent: "posts",
     })),
     []
