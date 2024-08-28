4733202f23c18af391e2512792f2736ea1edb95b
YZRay1119 <yao19991119@mopcon.org>
2024-08-28 17:59:21 +0800

    調整文章參數

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -9,6 +9,8 @@ import { categories, type Category } from "@/categories";
 export default async function Posts() {
   const posts = await getPosts();
 
+  const isPublishedPosts = posts.filter((post) => post.isPublished);
+
   return (
     <section className="mx-auto w-11/12 lg:w-[70%] py-8 min-h-svh">
       <CategoryButtonGroup>
@@ -21,7 +23,7 @@ export default async function Posts() {
           />
         ))}
       </CategoryButtonGroup>
-      <PostsList posts={posts} />
+      <PostsList posts={isPublishedPosts} />
     </section>
   );
 }
